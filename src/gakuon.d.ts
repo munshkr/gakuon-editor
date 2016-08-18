0a2b0de3b5f35fdcc253e47a76167bcc9f79ab66
Damián Silvani <munshkr@gmail.com>
2016-08-18 00:15:38 -0300

    Define type definitions for gakuon

---
+++ src/gakuon.d.ts
@@ -0,0 +1,36 @@
+declare module "gakuon" {
+  export class Parser {
+    parse(mmlString: string): any
+  }
+
+  export class Document {
+    constructor(source: string);
+
+    directives: any[];
+    commands: any[];
+    initialState: any;
+    AST: any;
+  }
+
+  export interface CodeGeneratorOptions {
+    debug?: boolean;
+    player?: boolean;
+  }
+
+  export class CodeGenerator {
+    constructor(document: string, options: CodeGeneratorOptions);
+
+    generate(): string;
+  }
+
+  export interface CompilerOptions extends CodeGeneratorOptions {
+    debug?: boolean;
+    player?: boolean;
+  }
+
+  export class Compiler {
+    constructor(options: CompilerOptions);
+
+    compile(source: string): string;
+  }
+}
