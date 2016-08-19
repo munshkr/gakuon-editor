45553011aba98da2db7bc84523dcfcd23dbea440
Damián Silvani <munshkr@gmail.com>
2016-08-19 20:46:35 -0300

    Enable source maps

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ module.exports = {
   output: {
     filename: './dist/bundle.js'
   },
+  devtool: 'source-map',
   resolve: {
     extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js']
   },
