e00cb121a4425a5839f6d74eca15e8f4a330481b
Damián Silvani <munshkr@gmail.com>
2016-08-22 00:46:08 -0300

    Better type definitions for jssid

--- src/jssid.d.ts
+++ src/jssid.d.ts
@@ -3,5 +3,38 @@ declare module "jssid" {
   function playSID(sidurl: string, subtune: number): void;
 
   export
-  function jsSID(bufferlen: number, background_noise: number): any;
+  class jsSID {
+    author: string;
+    sourcecode: string;
+    version: string;
+    year: string;
+
+    constructor(bufferlen: number, background_noise: number);
+
+    /* Methods */
+    loadstart(sidurl: string, subt: number): void;
+    loadinit(sidurl: string, subt: number): void;
+    loadinitdata(data: Uint8Array, subt: number): void;
+    start(subt: number): void;
+    playcont(): void;
+    pause(): void;
+    stop(): void;
+
+    /* Getters */
+    gettitle(): string;
+    getauthor(): string;
+    getinfo(): string;
+    getsubtunes(): number;
+    getprefmodel(): string;
+    getmodel(): string;
+    getoutput(): number;
+    getplaytime(): number;
+
+    /* Setters */
+    setmodel(model: string): void;
+    setvolume(vol: number): void;
+    setloadcallback(fname: any): void;
+    setstartcallback(fname: any): void;
+    setendcallback(fname: any, seconds: number): void;
+  }
 }
